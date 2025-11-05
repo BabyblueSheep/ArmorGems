a13d4d1db2d223380a838a7bad7d88bb5aafd68d
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2025-11-04 20:47:50 -0600

    added debug automatically generated gems

---
+++ Content/ModdedArmorGem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static ArmorGems.Content.ModdedArmorGemGenerator;
+
+namespace ArmorGems.Content
+{
+    public class ModdedArmorGem : ModItem
+    {
+        public ArmorSet armorSet;
+        public string header;
+
+        protected override bool CloneNewInstances => true;
+        //TODO: remix has a mod preface like "CalamityMod/" before its sticky/bouncy rogue items. add that here, so the mods that add Terra sets arent fucked
+        public override string Name => header + " Armor Gem";
+        public override string Texture => "ArmorGems/Content/BaseArmorGem";
+
+        public ModdedArmorGem(ArmorSet armorSet, string header)
+        {
+            this.armorSet = armorSet;
+            this.header = header;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            tooltips.Add(new TooltipLine("HelmetTooltip", "Helmets:"));
+            foreach (ModItem item in armorSet.helmets)
+            {
+                tooltips.Add(new TooltipLine(item.Name + "Tooltip", item.Name));
+            }
+            tooltips.Add(new TooltipLine("ChestplateTooltip", "Chestplates:"));
+            foreach (ModItem item in armorSet.chestplates)
+            {
+                tooltips.Add(new TooltipLine(item.Name + "Tooltip", item.Name));
+            }
+            tooltips.Add(new TooltipLine("LeggingsTooltip", "Leggings:"));
+            foreach (ModItem item in armorSet.leggings)
+            {
+                tooltips.Add(new TooltipLine(item.Name + "Tooltip", item.Name));
+            }
+        }
+    }
+}
---
+++ Content/ModdedArmorGemGenerator.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Build.Framework;
+using MonoMod.RuntimeDetour;
+using ReLogic.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace ArmorGems.Content
+{
+    internal class ModdedArmorGemGenerator : ModSystem
+    {
+        public static MethodInfo resizeMethod = typeof(ModContent).GetMethod("ResizeArrays", BindingFlags.Static | BindingFlags.NonPublic);
+        public delegate void orig_ResizeArrays(bool optional);
+
+        public static Hook collectArmorSetsHook;
+
+        public override void Load()
+        {
+            collectArmorSetsHook = new Hook(resizeMethod, ResizeArraysAndGetArmorSetShit);
+        }
+        
+        public static void ResizeArraysAndGetArmorSetShit(orig_ResizeArrays orig, bool unloading)
+        {
+            FieldInfo modLoading = typeof(Mod).GetField("loading", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (modLoading != null)
+            {
+                modLoading.SetValue(ModLoader.GetMod("ArmorGems"), true);
+                List<int> helmets = new();
+                List<int> chestplates = new();
+                List<int> leggings = new();
+                List<ArmorSet> armorSets = new();
+
+                // collect lists of helmets, chestplates, leggings
+                for (int i = ItemID.Count; i < ItemLoader.ItemCount; i++)
+                {
+                    ModItem item = ItemLoader.GetItem(i);
+                    AutoloadEquip equip = item.GetType().GetAttribute<AutoloadEquip>();
+                    if (equip != null)
+                    {
+                        EquipType[] equipTypes = equip.equipTypes;
+                        foreach (EquipType equipe in equipTypes)
+                        {
+                            if (equipe == EquipType.Head)
+                            {
+                                //ArmorGems.instance.Logger.Info("item loaded: " + item.Name + " as helmet");
+                                helmets.Add(item.Type);
+                            }
+                            if (equipe == EquipType.Body)
+                            {
+                                //ArmorGems.instance.Logger.Info("item loaded: " + item.Name + " as chestplate");
+                                chestplates.Add(item.Type);
+                            }
+                            if (equipe == EquipType.Legs)
+                            {
+                                //ArmorGems.instance.Logger.Info("item loaded: " + item.Name + " as leggings");
+                                leggings.Add(item.Type);
+                            }
+                        }
+                    }
+                }
+
+                // compare armor pieces to each other and get list of armor sets
+                for (int i = 0; i < helmets.Count; i++)
+                {
+                    ModItem helmet = ItemLoader.GetItem(helmets[i]);
+                    for (int j = 0; j < chestplates.Count; j++)
+                    {
+                        for (int k = 0; k < leggings.Count; k++)
+                        {
+                            if (helmet.IsArmorSet(helmet.Item, ItemLoader.GetItem(chestplates[j]).Item, ItemLoader.GetItem(leggings[k]).Item))
+                            {
+                                // cull sets that share 2 pieces into one
+                                bool addSet = true;
+                                foreach (ArmorSet set in armorSets)
+                                {
+                                    if (set.helmets[0] != helmet && set.chestplates[0] == ItemLoader.GetItem(chestplates[j]) && set.leggings[0] == ItemLoader.GetItem(leggings[k]))
+                                    {
+                                        set.helmets.Add(helmet);
+                                        addSet = false;
+                                    }
+                                    else if (set.helmets[0] == helmet && set.chestplates[0] != ItemLoader.GetItem(chestplates[j]) && set.leggings[0] == ItemLoader.GetItem(leggings[k]))
+                                    {
+                                        set.chestplates.Add(ItemLoader.GetItem(chestplates[j]));
+                                        addSet = false;
+                                    }
+                                    else if (set.helmets[0] == helmet && set.chestplates[0] == ItemLoader.GetItem(chestplates[j]) && set.leggings[0] != ItemLoader.GetItem(leggings[k]))
+                                    {
+                                        set.leggings.Add(ItemLoader.GetItem(leggings[k]));
+                                        addSet = false;
+                                    }
+                                }
+
+                                if (addSet)
+                                {
+                                    //ArmorGems.instance.Logger.Info("Armor Set: " + helmet.Name + " " + ItemLoader.GetItem(chestplates[j]).Name + " " + ItemLoader.GetItem(leggings[k]).Name);
+                                    armorSets.Add(new ArmorSet(helmet, ItemLoader.GetItem(chestplates[j]), ItemLoader.GetItem(leggings[k])));
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // add items to game
+                foreach (ArmorSet set in armorSets)
+                {
+                    ModdedArmorGem gem = new ModdedArmorGem(set, set.helmets[0].Name);
+                    ModContent.GetInstance<ArmorGems>().AddContent(gem);
+                }
+
+                modLoading.SetValue(ModLoader.GetMod("ArmorGems"), true);
+            }
+            orig(unloading);
+        }
+    }
+
+    public struct ArmorSet
+    {
+        public List<ModItem> helmets;
+        public List<ModItem> chestplates;
+        public List<ModItem> leggings;
+
+        #region Constructors
+        public ArmorSet(List<ModItem> helmets, List<ModItem> chestplates, List<ModItem> leggings)
+        {
+            this.helmets = helmets;
+            this.chestplates = chestplates;
+            this.leggings = leggings;
+        }
+
+        public ArmorSet(ModItem helmets, ModItem chestplates, ModItem leggings)
+        {
+            this.helmets = new List<ModItem> { helmets };
+            this.chestplates = new List<ModItem> { chestplates };
+            this.leggings = new List<ModItem> { leggings };
+        }
+        #endregion
+    }
+}
