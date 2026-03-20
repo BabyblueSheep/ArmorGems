11e8d334fb91173604b7a4b9253fce8a7166cb89
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-03-21 01:45:03 +0200

    begin refactor

--- ArmorGems.cs
+++ ArmorGems.cs
@@ -5,21 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
 
-namespace ArmorGems
+namespace ArmorGems;
+
+internal sealed class ArmorGems : Mod
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class ArmorGems : Mod
-	{
-		public static ArmorGems instance;
+    public static ArmorGems Instance { get; private set; }
 
-        public override void Load()
-        {
-			instance = this;
-        }
+    public override void Load()
+    {
+        Instance = this;
+    }
 
-        public override void Unload()
-        {
-            instance = null;
-        }
+    public override void Unload()
+    {
+        Instance = null;
     }
 }
